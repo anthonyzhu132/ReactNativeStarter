9dfff6af0cd33ed1449223b6e4c0e9c163e1ca20
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-07 20:29:40 -0700

    fixed text style font size

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const ComponentScreen = () => {
-  return <Text style={styles.text}>ComponentScreen Dank</Text>;
+  return <Text style={styles.textStyle}>ComponentScreen Dank</Text>;
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 50
   }
 });
 
