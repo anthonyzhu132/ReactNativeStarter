27587a688b6fb5a074e1d499387355c8efe8a4b8
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-08 16:21:29 -0700

    added age to display

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,22 +4,20 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 const ListScreen = () => {
   
   const friends = [
-    { name: 'Friend #1'},
-    { name: 'Friend #2'},
-    { name: 'Friend #3'},
-    { name: 'Friend #4'},
-    { name: 'Friend #5'},
-    { name: 'Friend #6'},
+    { name: 'Friend #1', age: "15"},
+    { name: 'Friend #2', age: "12"},
+    { name: 'Friend #3', age: "14"},
+    { name: 'Friend #4', age: "11"},
+    { name: 'Friend #5', age: "10"},
+    { name: 'Friend #6', age: "199"},
   ]
 
   return (
     <FlatList
-      horizontal
-      showsHorizontalScrollIndicator={false}
       keyExtractor={friend => friend.name}
       data ={friends}
       renderItem={({ item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>
+        return <Text style={styles.textStyle}>{item.name} Age {item. age}</Text>
       }}
     />
   );
@@ -27,7 +25,7 @@ const ListScreen = () => {
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical: 50
+    marginVertical: 25
   }
 });
 
