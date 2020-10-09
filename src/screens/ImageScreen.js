fa275cac8e0d354e81add2f4e8e087ec83dbc627
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-08 17:28:20 -0700

    added image score passed through parent props of ImageScreen to ImageDetail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,9 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} />
-      <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')}/>
-      <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')}/>
+      <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score="69"/>
+      <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score="19"/>
+      <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score="29"/>
     </View>
   );
 };
