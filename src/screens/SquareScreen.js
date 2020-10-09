1db46ab9205e5b1e4e8808b4b2a0e89b42a7034c
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-08 23:00:34 -0700

    added cases to switch colours

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,30 +8,45 @@ const SquareScreen = () => {
   const [blue, setBlue] = useState(0);
   const [green, setGreen] = useState(0);
 
+  const COLOUR_INCREMENT = 10
 
-  const SETTER = `rgb(${red}, ${green}, ${blue})`
-  const COLOR_INCREMENT = 10
+  const setColour = (colour, change) => {
+    // color = 'red', 'green', 'blue
+    // change === +15, -15
+
+    switch(colour) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case 'green':
+        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+        return;
+    case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
+    }
+  }
 
   return (
     <View>
       <ColourCounter
       colour="Red"
-      onIncrease={() => setRed(red + COLOR_INCREMENT)}
-      onDecrease={() => setRed(red - COLOR_INCREMENT)}
+      onIncrease={() => setColour('red', COLOUR_INCREMENT)}
+      onDecrease={() => setColour('red', -1 * COLOUR_INCREMENT)}
       />
 
       <ColourCounter
       colour="Green"
-      onIncrease={() => setGreen(green +COLOR_INCREMENT)}
-      onDecrease={() => setGreen(green -COLOR_INCREMENT)}
+      onIncrease={() => setColour('green', +COLOUR_INCREMENT)}
+      onDecrease={() => setColour('green', -1 * COLOUR_INCREMENT)}
       />
 
       <ColourCounter
       colour="Blue"
-      onIncrease={() => setBlue(blue +COLOR_INCREMENT)}
-      onDecrease={() => setBlue(blue -COLOR_INCREMENT)}
+      onIncrease={() => setColour('blue', +COLOUR_INCREMENT)}
+      onDecrease={() => setColour('blue', -1 * COLOUR_INCREMENT)}
       />
-      <View style={{ height: 100, width: 100, backgroundColor: SETTER }}/>
+      <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
     </View>
   );
 };
