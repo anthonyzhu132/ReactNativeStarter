0886aa34342ffa0c783cb970520a6dbe5a10ea56
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-08 17:15:39 -0700

    added added ImageDetail.js and added asset photos

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageDetail = ({ title }) => {
+  return <Text>{title}</Text>
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 45
+  },
+});
+
+export default ImageDetail;
