1a59db8e5c5ed431d9dc278815b133c89a195b71
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-08 21:20:32 -0700

    used setState to generate random RGB square using a random colour generator

--- src/screens/ColourScreen.js
+++ src/screens/ColourScreen.js
@@ -2,11 +2,28 @@ import React, { useState } from "react";
 import { View, StyleSheet, Button } from "react-native";
 
 const ColourScreen = () => {
+
+  const [colour, setColour] = useState([]);
+
   return (
     <View>
-      <Button title="Add Colour"/>
+      <Button 
+      title="Add Colour"
+      onPress={() => {
+        setColour([...colour, randomRgb()])
+      }}
+      />
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }}/>
     </View>
   );
+}
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 const styles = StyleSheet.create({});
