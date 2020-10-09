f5a73e6f794a4985a29c761ccec7c53073b9d6ba
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-09 14:04:04 -0700

    using useState to give child component text input

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+
+  const [name, setName] = useState('');
+  console.log(name);
+
   return (
     <View>
-      <TextInput style={styles.inputStyle} />
+      <TextInput 
+      style={styles.inputStyle}
+      autoCapitalize="none" 
+      autoCorrect={false}
+      value={name}
+      onChangeText={(newText) => setName(newText)}
+      />
     </View>
   );
 };
