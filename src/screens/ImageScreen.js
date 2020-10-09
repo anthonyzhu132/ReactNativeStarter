68b14b62561eba1f0f826568c28cc3282cb881ca
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-08 17:29:22 -0700

    fixed indentation

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,21 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score="69"/>
-      <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score="19"/>
-      <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score="29"/>
+      <ImageDetail 
+      title="Forest" 
+      imageSource={require('../../assets/forest.jpg')} 
+      score="69"
+      />
+      <ImageDetail 
+      title="Beach" 
+      imageSource={require('../../assets/beach.jpg')} 
+      score="19"
+      />
+      <ImageDetail 
+      title="Mountain" 
+      imageSource={require('../../assets/mountain.jpg')} 
+      score="29"
+      />
     </View>
   );
 };
